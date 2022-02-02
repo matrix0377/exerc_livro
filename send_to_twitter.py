e0fd27e410addbd1651f9deb1aa36201bf2480a6
Davi Augusto <davi.cianorte@gmail.com>
2022-02-01 23:19:45 -0300

    Implementando mais arquivos

--- send_to_twitter.py
+++ send_to_twitter.py
@@ -1,34 +1,33 @@
 '''
+site: http://beans.itcarlow.ie/prices.html (não oficial)
 Envio de msg da cotação para o twitter
 '''
 
+from tkinter import Y
 import urllib.request
 import time
 
-def set_password():
-    password="C8H10N402"
 
-set_password()
+password="matrix@@27"
 
-password="C8H10N402"
 
 def send_to_twitter(msg):
     password_manager = urllib.request.HTTPPasswordMgr()
     
-    password_manager.add_password("Twitter API", "http://twitter.com/statuses", "...", "...")
+    password_manager.add_password("Twitter API", "http://twitter.com/statuses", "@AugustoLivro", password)
     http_handler = urllib.request.HTTPBasicAuthHandler(password_manager)
     page_opener = urllib.request.build_opener(http_handler)
     urllib.request.install_opener(page_opener)
-    params = urllib.parse.urlencode( {'status': msg})
+    params = urllib.parse.urlencode( {'status': msg} )
     resp = urllib.request.urlopen("http://twitter.com/statuses/update.json", params)
     resp.read()
     
     
     
 def get_price():
-    page = urllib.request.urlopen("http://www.beans-r-us.biz/prices.html")    
+    page = urllib.request.urlopen("http://beans.itcarlow.ie/prices.html")    
     text = page.read().decode("utf8")
-    where = text.find('>$')
+    where = text.find('= $')
     start_of_price = where + 2
     end_of_price = start_of_price + 4
     return float(text[start_of_price:end_of_price])
@@ -43,6 +42,8 @@ else:
         time.sleep(900)
         price = get_price()
         send_to_twitter("Buy!")
+print('Cotação enviada com sucesso!!')
+
 
 
     
