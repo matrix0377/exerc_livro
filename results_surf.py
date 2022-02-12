075c2d4a82e4ebb2cd6fb0d130d8cdcfd5faeafd
Davi Augusto <droidcenter0377@gmail.com>
2022-02-12 17:49:51 -0300

    Surf-A-Thon

--- results_surf.py
+++ results_surf.py
@@ -7,15 +7,31 @@ result_f.close()
 print("The highest score was:")
 print(highest_score)'''
 
+scores = []
 
-highest_score = 0
 result_f = open("results.txt")
+
 for line in result_f:
     (name, score) = line.split()
-    if float(score) > highest_score:
-        highest_score = float(score)
+    scores.append(float(score))
 result_f.close()
-print("The highest score was:")
-print(highest_score)
 
+scores.sort()
+scores.reverse()
+
+print(' ')
+print('-*' * 25)
+print("Surf-A-Thon")
+print('-*' * 25)
+print('=' * 25)
+print("The TOP Scores were:")
+print('=' * 25)
+print(' ')
+print(scores[0])
+print("\n")
+print(scores[1])
+print("\n")
+print(scores[2])
+print("\n")
+print('-*' * 25)
 
