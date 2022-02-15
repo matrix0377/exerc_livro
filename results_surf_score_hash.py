d572a0997ed252ae75c9f4be175348e6f88b8328
Davi Augusto <droidcenter0377@gmail.com>
2022-02-15 16:04:17 -0300

    aprimoramento

--- results_surf_score_hash.py
+++ results_surf_score_hash.py
@@ -7,35 +7,35 @@ result_f.close()
 print("The highest score was:")
 print(highest_score)'''
 
-scores = []
-names = []
+scores = {}
 
 result_f = open("results.txt")
 
 for line in result_f:
     (name, score) = line.split()
-    scores.append(float(score))
-    names.append(name)
+    scores[score] = name
 result_f.close()
-
-scores.sort()
-scores.reverse()
-names.sort()
-names.reverse()
 
 print(' ')
 print('-*' * 25)
 print("Surf-A-Thon")
+print("The higuest Scores were:")
+print('-*' * 25)
+print(' ')
+for each_score in sorted(scores.keys(), reverse = True):
+    print('Surfer ' + scores[each_score] + ' scored ' + each_score)
+    print(' ')
+
+print("\n")
+print('-*' * 25)
+
+'''
 print('-*' * 25)
 print('=' * 25)
 print("The higuest Scores were:")
 print('=' * 25)
 print(' ')
-print(names[0] + ' with ' + str(scores[0]))
-print("\n")
-print(names[1] + ' with ' + str(scores[1]))
-print("\n")
-print(names[2] + ' with ' + str(scores[2]))
-print("\n")
-print('-*' * 25)
+'''
+
+
 
