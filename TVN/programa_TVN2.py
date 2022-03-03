800ecb6782e24a72f9b25cb1439afad79b3932da
Davi Augusto <droidcenter0377@gmail.com>
2022-03-03 15:31:23 -0300

    inclusao de exercicio

--- TVN/programa_TVN2.py
+++ TVN/programa_TVN2.py
@@ -6,10 +6,13 @@ import pygame.mixer
 
 # a função "wait_finish()" faz um laço até o método "get_busy()"
 # do canal retornar False
+
+
 def wait_finish(channel):
     while channel.get_busy():
         pass
-    
+
+
 # cria um objeto mixer e inicializa o sistema de som de pygame
 sounds = pygame.mixer
 sounds.init()
@@ -24,12 +27,12 @@ prompt = "Press 1 for Correct, 2 for Wrong or 0 to Quit: "
 # iniciando valores
 number_asked = 0
 number_correct = 0
-number_wrong = 0 
+number_wrong = 0
 
 # solicite ao apresentador
 choice = input(prompt)
 
- 
+
 while choice != "0":
     # Se a resposta for correta, aumente os contadores e então
     # reproduza o devido som.
@@ -37,18 +40,17 @@ while choice != "0":
         number_asked = number_asked + 1
         number_correct = number_correct + 1
         wait_finish(correct_s.play())
-        
+
     # Se a resposta for incorreta, aumente os contadores e então
-    # reproduza o efeito sonoro        
+    # reproduza o efeito sonoro
     if choice == "2":
         number_asked = number_asked + 1
         number_wrong = number_wrong + 1
         wait_finish(wrong_s.play())
     choice = input(prompt)
-    
-print("==" * 25)    
+
+print("==" * 25)
 print("You asked " + str(number_asked) + " questions.")
 print("  -->>>> " + str(number_correct) + " were correctly answered.")
 print("  -->>>> " + str(number_wrong) + " were answered incorrectly.")
 print("-=*" * 25)
-
---
+++ tvn.pyw
@@ -0,0 +1,45 @@
+from tkinter import *
+
+import pygame.mixer
+
+sounds = pygame.mixer
+sounds.init()
+
+correct_s = sounds.Sound("correct.wav")
+wrong_s = sounds.Sound("wrong.wav")
+
+number_asked = 0
+number_correct = 0
+number_wrong = 0
+
+def play_correct_sound():
+    global number_correct
+    global number_asked
+    number_correct = number_correct + 1
+    number_asked = number_asked + 1
+    correct_s.play()
+    
+    
+def play_wrong_sound():
+    global number_wrong
+    global number_asked
+    number_wrong = number_wrong + 1
+    number_asked = number_asked + 1
+    wrong_s.play()
+    
+
+app = Tk()
+app.title("TVN Game Show")
+app.geometry('300x100+200+100')
+b1 = Button(app, text = 'Correct!', width = 10, command = play_correct_sound)
+b1.pack(side = 'left', padx = 10, pady = 10)
+
+b2 = Button(app, text='wrong!', width=10, command=play_wrong_sound)
+b2.pack(side='right', padx=10, pady=10)
+
+app.mainloop()
+
+print("You asked " + str(number_asked) + " questions.")
+print(str(number_correct) + " were correctly answered.")
+print(str(number_wrong) + " were answered incorrectly.")
+
